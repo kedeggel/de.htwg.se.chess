5384b45c029a3b81cc5846d037d16dd5c60fd791
kedeggel <kedeggel@htwg-konstanz.de>
2016-04-15 10:05:12 +0200

    added class FieldConstants with symbolic names for fields on the chessboard

---
+++ de.htwg.se.chess/src/de/htwg/chess/model/FieldConstants.java
@@ -0,0 +1,20 @@
+package de.htwg.chess.model;
+
+public final class FieldConstants {
+	public final static int A = 0;
+	public final static int B = 1;
+	public final static int C = 2;
+	public final static int D = 3;
+	public final static int E = 4;
+	public final static int F = 5;
+	public final static int G = 6;
+	public final static int H = 7;
+	public final static int ONE = 0;
+	public final static int TWO = 1;
+	public final static int THREE = 2;
+	public final static int FOUR = 3;
+	public final static int FIVE = 4;
+	public final static int SIX = 5;
+	public final static int SEVEN = 6;
+	public final static int EIGHT = 7;
+}
