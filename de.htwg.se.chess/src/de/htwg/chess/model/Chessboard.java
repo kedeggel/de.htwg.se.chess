ec9c5f53d3b2ac61e7cdf073dcdf45f20434410a
Sascha Nunes <sanunes@htwg-konstanz.de>
2016-05-06 15:51:41 +0200

    moved "ChessboardCreator"-functionality into init-method of "Chessboard"

--- de.htwg.se.chess/src/de/htwg/chess/model/Chessboard.java
+++ de.htwg.se.chess/src/de/htwg/chess/model/Chessboard.java
@@ -1,6 +1,5 @@
 package de.htwg.chess.model;
 
-
 /**
  * 
  * @author Sascha Nunes, Kevin Deggelmann (team 10)
@@ -13,10 +12,18 @@ public class Chessboard {
 	private Player black;
 
 	public Chessboard() {
-		chessboard = new ChessboardCreater().getChessboard();
+		initChessboard();
 	}
 
-	
+	private void initChessboard() {
+		chessboard = new Position[8][8];
+		for (char x = 'A'; x <= 'H'; x++) {
+			for (int y = 1; y <= 8; y++) {
+				chessboard[x - 'A'][y - 1] = new Position(x, y);
+			}
+		}
+	}
+
 	public Position[][] getChessboard() {
 		return chessboard;
 	}
@@ -37,7 +44,5 @@ public class Chessboard {
 				sb.append(chessboard[i][j] + ": " + chessboard[i][j].getChesspiece() + "\n");
 		return sb.toString();
 	}
-	
-	
 
 }
