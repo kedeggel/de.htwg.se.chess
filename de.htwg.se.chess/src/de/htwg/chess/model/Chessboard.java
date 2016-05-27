7b61b7f493dcff62b93d0e8460749a6cdfe510c7
sanunes <sanunes@htwg-konstanz.de>
2016-05-27 12:05:20 +0200

    renamed field-matrix in Chessboard to 'board'

--- de.htwg.se.chess/src/de/htwg/chess/model/Chessboard.java
+++ de.htwg.se.chess/src/de/htwg/chess/model/Chessboard.java
@@ -9,7 +9,7 @@ import static de.htwg.chess.model.Team.Color.*;
  */
 
 public class Chessboard {
-	private Field[][] chessboard;
+	private Field[][] board;
 	private Team[] teamlist;
 	private MoveChecker moveChecker;
 
@@ -20,10 +20,10 @@ public class Chessboard {
 	}
 
 	private void initChessboard() {
-		chessboard = new Field[8][8];
+		board = new Field[8][8];
 		for (char x = 'A'; x <= 'H'; x++) {
 			for (int y = 1; y <= 8; y++) {
-				chessboard[x - 'A'][y - 1] = new Field(x, y);
+				board[x - 'A'][y - 1] = new Field(x, y);
 			}
 		}
 	}
@@ -35,7 +35,7 @@ public class Chessboard {
 	}
 
 	public Field getField(int x, int y) {
-		return chessboard[x][y];
+		return board[x][y];
 	}
 
 	public Team getTeam(Team.Color color) {
@@ -52,7 +52,7 @@ public class Chessboard {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < 8; i++)
 			for (int j = 0; j < 8; j++)
-				sb.append(chessboard[i][j] + ": " + chessboard[i][j].getChesspiece() + "\n");
+				sb.append(board[i][j] + ": " + board[i][j].getChesspiece() + "\n");
 		return sb.toString();
 	}
 
