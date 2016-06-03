834f53666058cc089d38ddda5521974ae1be15ca
kedeggel <kedeggel@htwg-konstanz.de>
2016-06-03 09:51:54 +0200

    added basic methods to Controller

--- de.htwg.se.chess/src/de/htwg/chess/Chess.java
+++ de.htwg.se.chess/src/de/htwg/chess/Chess.java
@@ -1,8 +1,15 @@
 package de.htwg.chess;
 
+import de.htwg.chess.controller.ChessController;
 
 public class Chess {
-	
+	private ChessController controller;
+
+	private Chess() {
+		controller = new ChessController();
+	}
+
 	public static void main(String[] args) {
+
 	}
 }
