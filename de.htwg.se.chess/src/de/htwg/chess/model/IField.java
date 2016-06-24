a7e5ec334ce6f6f07d3657deff59ebe5bc4545d0
sanunes <sanunes@htwg-konstanz.de>
2016-06-24 12:38:30 +0200

    added JavaDoc for "IField" and "ITeam"

--- de.htwg.se.chess/src/de/htwg/chess/model/IField.java
+++ de.htwg.se.chess/src/de/htwg/chess/model/IField.java
@@ -1,20 +1,47 @@
 package de.htwg.chess.model;
 
-
 public interface IField {
 
+	/**
+	 * Returns the chesspiece which is placed on this field.
+	 * 
+	 * @return chesspiece on this field. If this field is empty, "null" is
+	 *         returned.
+	 */
 	IChesspiece getChesspiece();
 
+	/**
+	 * Places a specified chesspiece on this field, clearing the field, it used
+	 * to be placed before and telling the chesspiece its new position.
+	 * 
+	 * @param cp
+	 *            is the chesspiece which will be moved and updated.
+	 */
 	void setChesspiece(IChesspiece cp);
 
+	/**
+	 * Returns the letter-part of this chessfields' coordinates.
+	 * 
+	 * @return x-Value of this chessfield as a Char.
+	 */
 	char getX();
 
+	/**
+	 * Returns the number-part of this chessfields' coordinates.
+	 * 
+	 * @return y-Value of this chessfield as an int.
+	 */
 	int getY();
 
+	/**
+	 * Checks whether a field is occupied by a chesspiece or empty.
+	 * 
+	 * @return true, if the fields' chesspiece is not null. false, if the
+	 *         fields' chesspiece is null.
+	 */
 	boolean isFieldOccupied();
 
 	@Override
 	String toString();
-
 
 }
--- de.htwg.se.chess/src/de/htwg/chess/model/ITeam.java
+++ de.htwg.se.chess/src/de/htwg/chess/model/ITeam.java
@@ -6,20 +6,68 @@ import de.htwg.chess.model.impl.Team.Color;
 
 public interface ITeam {
 
+	/**
+	 * Returns the opposing teams color, represented by a enum with 2
+	 * Color-Entries.
+	 * 
+	 * @return Color of the opponent team.
+	 */
 	Color opponent();
 
+	/**
+	 * Returns a List of all the chesspieces on the current Teams' side.
+	 * 
+	 * @return List of chesspieces.
+	 */
 	List<IChesspiece> getPieceList();
 
+	/**
+	 * Moves a chesspiece to a designated field on the board.
+	 * 
+	 * @param cp
+	 *            is the chesspiece which is about to be moved.
+	 * @param target
+	 *            is the target field location.
+	 */
 	void move(IChesspiece cp, IField target);
 
+	/**
+	 * Return the "King"-chesspiece.
+	 * 
+	 * @return Chesspiece "King"
+	 */
 	IChesspiece getKing();
 
+	/**
+	 * Returns the teams color, represented by a enum with 2 Color-Entries.
+	 * 
+	 * @return Color of the Team
+	 */
 	Color getColor();
 
+	/**
+	 * Removes a chesspiece on the board, settting its position to null and
+	 * deleting it from the List of Chesspieces for the corresponding team.
+	 * 
+	 * @param cp
+	 *            is the chesspiece which gets deleted.
+	 */
 	void removeChesspiece(IChesspiece cp);
 
+	/**
+	 * Adds a specified chesspiece to the team, adding it to the pieceList and
+	 * placing it on a free field on the board
+	 * 
+	 * @param cp
+	 *            is the chesspiece, which will be added.
+	 */
 	void addChesspiece(IChesspiece cp);
 
+	/**
+	 * Updates the possible moves of each chesspiece in this team. Allowed
+	 * moving-paterns are defined by the official chess-rules and can be checked
+	 * at "https://help.gnome.org/users/glchess/stable/chess-rules.html.en"
+	 */
 	void updatePosMoves();
 
 }
