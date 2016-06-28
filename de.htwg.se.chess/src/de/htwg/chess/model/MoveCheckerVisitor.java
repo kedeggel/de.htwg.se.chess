ebb044cb5b62252f0965987f7c19c9879c86cd09
sanunes <sanunes@htwg-konstanz.de>
2016-06-28 18:22:05 +0200

    added JavaDoc for Interface "MoveCheckerVisitor"

--- de.htwg.se.chess/src/de/htwg/chess/model/MoveCheckerVisitor.java
+++ de.htwg.se.chess/src/de/htwg/chess/model/MoveCheckerVisitor.java
@@ -2,18 +2,52 @@ package de.htwg.chess.model;
 
 import java.util.List;
 
+/**
+ * All the rules for move-validations in this interfaces' implementations are
+ * defined by the official chess-rules.
+ */
 public interface MoveCheckerVisitor {
 
+	/**
+	 * @param field
+	 *            is the initial position of the queen.
+	 * @return List of IFields the queen can move to.
+	 */
 	public List<IField> checkQueen(IField field);
 
+	/**
+	 * @param field
+	 *            is the initial position of the king.
+	 * @return List of IFields the king can move to.
+	 */
 	public List<IField> checkKing(IField field);
 
+	/**
+	 * @param field
+	 *            is the initial position of the rook.
+	 * @return List of IFields the rook can move to.
+	 */
 	public List<IField> checkRook(IField field);
 
+	/**
+	 * @param field
+	 *            is the initial position of the bishop.
+	 * @return List of IFields the bishop can move to.
+	 */
 	public List<IField> checkBishop(IField field);
 
+	/**
+	 * @param field
+	 *            is the initial position of the knight.
+	 * @return List of IFields the knight can move to.
+	 */
 	public List<IField> checkKnight(IField field);
 
+	/**
+	 * @param field
+	 *            is the initial position of the pawn.
+	 * @return List of IFields the pawn can move to.
+	 */
 	public List<IField> checkPawn(IField field);
 
 }
