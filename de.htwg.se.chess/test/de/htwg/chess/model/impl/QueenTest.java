fa86b8ea06d8fe98be8dcdf054aa6c0a6c923fb7
sanunes <sanunes@htwg-konstanz.de>
2016-06-28 16:34:49 +0200

    added "QueenTest"

---
+++ de.htwg.se.chess/test/de/htwg/chess/model/impl/QueenTest.java
@@ -0,0 +1,107 @@
+package de.htwg.chess.model.impl;
+
+import static de.htwg.chess.model.impl.FieldConstants.A;
+import static de.htwg.chess.model.impl.FieldConstants.B;
+import static de.htwg.chess.model.impl.FieldConstants.C;
+import static de.htwg.chess.model.impl.FieldConstants.D;
+import static de.htwg.chess.model.impl.FieldConstants.E;
+import static de.htwg.chess.model.impl.FieldConstants.EIGHT;
+import static de.htwg.chess.model.impl.FieldConstants.F;
+import static de.htwg.chess.model.impl.FieldConstants.FIVE;
+import static de.htwg.chess.model.impl.FieldConstants.FOUR;
+import static de.htwg.chess.model.impl.FieldConstants.G;
+import static de.htwg.chess.model.impl.FieldConstants.H;
+import static de.htwg.chess.model.impl.FieldConstants.ONE;
+import static de.htwg.chess.model.impl.FieldConstants.SEVEN;
+import static de.htwg.chess.model.impl.FieldConstants.SIX;
+import static de.htwg.chess.model.impl.FieldConstants.THREE;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.htwg.chess.model.Color;
+import de.htwg.chess.model.IChessboard;
+import de.htwg.chess.model.IChesspiece;
+import de.htwg.chess.model.IField;
+import junit.framework.TestCase;
+
+public class QueenTest extends TestCase {
+	IChessboard chessboard;
+	IChesspiece queen_test;
+	IChesspiece piece_friendly;
+	IChesspiece piece_enemy;
+	List<IField> checklist;
+
+	@Before
+	public void setUp() {
+		chessboard = new Chessboard();
+		queen_test = new Queen(Color.WHITE, chessboard.getField(D, FIVE));
+		piece_friendly = new Pawn(Color.WHITE, chessboard.getField(G, FIVE));
+		piece_enemy = new Pawn(Color.BLACK, chessboard.getField(B, FIVE));
+		checklist = new LinkedList<>();
+	}
+
+	@Test
+	public void testPossibleMoves() {
+		/**
+		 * right friendly, left enemy
+		 */
+		checklist.add(chessboard.getField(C, FIVE));
+		checklist.add(chessboard.getField(B, FIVE));
+		checklist.add(chessboard.getField(E, FIVE));
+		checklist.add(chessboard.getField(F, FIVE));
+		checklist.add(chessboard.getField(D, SIX));
+		checklist.add(chessboard.getField(D, SEVEN));
+		checklist.add(chessboard.getField(D, FOUR));
+		checklist.add(chessboard.getField(D, THREE));
+		checklist.add(chessboard.getField(C, SIX));
+		checklist.add(chessboard.getField(B, SEVEN));
+		checklist.add(chessboard.getField(E, FOUR));
+		checklist.add(chessboard.getField(F, THREE));
+		checklist.add(chessboard.getField(C, FOUR));
+		checklist.add(chessboard.getField(B, THREE));
+		checklist.add(chessboard.getField(E, SIX));
+		checklist.add(chessboard.getField(F, SEVEN));
+		chessboard.getTeam(Color.WHITE).addChesspiece(queen_test);
+		chessboard.getTeam(Color.WHITE).addChesspiece(piece_friendly);
+		chessboard.getTeam(Color.BLACK).addChesspiece(piece_enemy);
+
+		assertTrue(checklist.containsAll(queen_test.getPossibleMoves()));
+
+		/**
+		 * left friendly, right enemy
+		 */
+		piece_friendly.setField(chessboard.getField(B, FIVE));
+		piece_enemy.setField(chessboard.getField(G, FIVE));
+		checklist.add(chessboard.getField(G, FIVE));
+		checklist.remove(chessboard.getField(B, FIVE));
+		chessboard.updateTeams();
+		assertTrue(checklist.containsAll(queen_test.getPossibleMoves()));
+
+		/**
+		 * down enemy, up friendly
+		 */
+		piece_friendly.setField(chessboard.getField(D, SIX));
+		piece_enemy.setField(chessboard.getField(D, THREE));
+		checklist.add(chessboard.getField(A, FIVE));
+		checklist.add(chessboard.getField(B, FIVE));
+		checklist.add(chessboard.getField(H, FIVE));
+		checklist.remove(chessboard.getField(D, SIX));
+		checklist.remove(chessboard.getField(D, SEVEN));
+		chessboard.updateTeams();
+		assertTrue(checklist.containsAll(queen_test.getPossibleMoves()));
+		assertTrue(chessboard.getField(A, ONE).getChesspiece().getPossibleMoves().isEmpty());
+		assertTrue(chessboard.getField(A, EIGHT).getChesspiece().getPossibleMoves().isEmpty());
+
+	}
+
+	@Test
+	public void testToSymbole() {
+		assertTrue("\u2655".equals(new Queen(Color.WHITE, chessboard.getField(3, 3)).toSymbole()));
+		assertTrue("\u265B".equals(new Queen(Color.BLACK, chessboard.getField(3, 3)).toSymbole()));
+	}
+	
+}
